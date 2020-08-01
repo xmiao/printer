c3270e571150ed3214b6ce6a10c0a5be08ec5b21
Xijiang <mxj@winning.com.cn>
2020-08-01 19:27:25 +0800

    2nd

--- public/vue/index.js
+++ public/vue/index.js
@@ -1,11 +1,66 @@
-import Vue from 'vue';
-import App from './vue/App';
+// import Vue from 'vue';
 
-Vue.config.productionTip = false;
+// import App from './vue/App';
+//
+// Vue.config.productionTip = false;
+//
+// /* eslint-disable no-new */
+// new Vue({
+//     el: '#app',
+//     template: '<App/>',
+//     components: {App}
+// });
+//
+//
+// 'use strict';
+// var User = httpVueLoader('./js/user.vue')
+// var Sidebar = httpVueLoader('./js/sidebar.vue')
+// var UserList = httpVueLoader('./js/user-list.vue')
+// var NamedWrapper = httpVueLoader('./js/named.vue')
+//
+//
+// /* Router and App setup: */
+// var routes = [
+//     {
+//         path: '/users',
+//         name: 'userList',
+//         component: UserList
+//     },
+//     {
+//         path: '/named',
+//         name: 'named',
+//         component: NamedWrapper,
+//         children: [{
+//             path: 'user/:userId',
+//             name: 'named_id',
+//             components: {user_details: User, sidebar: Sidebar},
+//             props: {user_details: true, sidebar: false}
+//         }]
+//     },
+//     {
+//         path: '/user/:userId',
+//         name: 'user',
+//         component: User,
+//         props: true
+//     }
+// ];
+//
+// var router = new VueRouter({
+//     routes: routes
+// });
 
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    template: '<App/>',
-    components: {App}
+    el: "#app",
+    data: {
+        "header": `<div style="font-size: 12pt; width: 100%; height: 30px;text-align: center;background-color: black; border-bottom: 1px solid black;margin: 0 1cm;\">人民医院门诊病历</div>`,
+        "footer": `<div style=\"font-size: 6pt;text-align: right;width: 100%;height: 20px;border-top: 1px solid black;color:black;font-family: Arial,serif;margin: 0 1cm;\">第<span class=\"pageNumber\"></span>页 共<span class=\"totalPages\"></span>页 打印日期<span class=\"date\"></span></div>`,
+        htmlFile: "",
+        format: "A4",
+        landscape: false,
+        doPrint: false
+    }
 });
+
+// new Vue({router: router})
+//     .$mount('#app');
