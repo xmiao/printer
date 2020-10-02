c523b98fc46c17e3fc3681bfabb9b6de7d355d47
Xijiang <mxj@winning.com.cn>
2020-10-03 05:17:38 +0800

    2nd

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,29 +1,27 @@
 import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import * as config from './config.json';
 
 Vue.use(VueRouter);
 
-const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const {routeList}: any = config;
+const modules: any = {
+    about: import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    home: import(/* webpackChunkName: "home" */ '../views/Home.vue')
+};
+
+const routes: Array<RouteConfig> = routeList
+    .map(({path, name, module}: any) => {
+        return {
+            path, name,
+            component: () => modules[module]
+        };
+    });
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;
