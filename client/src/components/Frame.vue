a37a09c05d1d24dbaa14316b6a5b86481a073770
Xijiang <mxj@winning.com.cn>
2020-10-05 09:24:51 +0800

    2nd

--- client/src/components/Frame.vue
+++ client/src/components/Frame.vue
@@ -1,20 +1,39 @@
 <template>
+  <component :is="component" v-if="component" :data="data"/>
 </template>
 
-<script lang="ts">
+<script>
 import {Component, Prop, Vue} from 'vue-property-decorator';
-import ElementUI from 'element-ui';
-
-Vue.prototype.$ELEMENT = {size: 'small', zIndex: 3000};
-Vue.use(ElementUI);
 
 @Component
 export default class Print extends Vue {
-  @Prop() private msg!: string;
+  @Prop() name = 'dynamic-link';
+  @Prop() data;
+  @Prop() type;
+
+  // data() {
+  //   return {
+  //     component: null,
+  //   }
+  // }
+
+  computed = {
+    loader() {
+      if (!this.type) {
+        return null
+      }
+      return () => import(`templates/${this.type}`)
+    },
+  }
+
+  mounted() {
+    this.loader()
+        .then(() => {
+          this.component = () => this.loader()
+        })
+        .catch(() => {
+          this.component = () => import('templates/default')
+        })
+  }
 }
-
 </script>
-
-<style lang="scss" scoped>
-
-</style>
