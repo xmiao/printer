215abf8caea495bb3a6453e028388cee2a68b7fa
Xijiang <mxj@winning.com.cn>
2020-10-07 13:10:34 +0800

    2nd

--- client/src/views/ChartingTableDefinition.vue
+++ client/src/views/ChartingTableDefinition.vue
@@ -15,7 +15,8 @@
 
       <el-container>
         <el-main>
-          <div v-html="html()"/>
+          some
+          <gen-print :def="chartingDef"></gen-print>
         </el-main>
       </el-container>
     </el-container>
@@ -23,141 +24,21 @@
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue} from 'vue-property-decorator';
+import {Component, Vue} from 'vue-property-decorator';
 import ElementUI from 'element-ui';
 import Choice from "@/components/Choice.vue";
 import WnFile from "@/components/File.vue";
 import chartingDef from "./charting.definition.json";
+import GenPrint from "./GenPrint.vue";
 
 Vue.prototype.$ELEMENT = {size: 'small', zIndex: 3000};
 Vue.use(ElementUI);
 
-class HandleDef {
-  private readonly def = [];
-
-  constructor(def: any) {
-    this.def = def;
-  }
-
-  psLabel(article: any) {
-    if (!article) return "";
-    const {text, html} = article;
-    if (html) return html;
-  }
-
-  psPanel(t: any) {
-    if (!t) return "";
-
-    const {tag, article, form, charting} = t;
-    if (tag !== "panel") {
-      return `<div>should be panel.</div>`
-    }
-
-    if (article || form || charting)
-      return [
-        this.psLabel(article),
-        this.psForm(form),
-        this.psCharting(charting)
-      ].join("");
-
-    return `<div style="border: 1px solid red">${JSON.stringify(t)}</div>`;
-  }
-
-  psForm(form: any) {
-    if (!form) return "";
-    const {fields = []} = form;
-    return fields
-        .map(({label, text, template}: any) => {
-          return `<div><div>${label}</div><div>${text}</div></div>`
-        })
-        .join("");
-  }
-
-  psCharting(charting: any) {
-    if (!charting) return "";
-    const {items = []} = charting;
-
-    function rc(node: any, pos: any): any {
-      const {items: i1 = []} = node || {};
-      let width = 0, height = 1;
-      const {x = 0, y = 0} = pos;
-      for (const i of i1) {
-        const {width: w2, height: h2} = rc(i, {x: x + width, y: y + 1});
-        width += w2;
-        height = Math.max(height, h2 + 1);
-      }
-      width = Math.max(1, width);
-      node._info = {width, height, x, y};
-      return {width, height, x, y};
-    }
-
-    const {width: totalWidth, height: totalHeight} = rc(charting, {});
-    const rowData: any[] = [];
-
-    function rc2(node: any): any {
-      const {items: i1 = [], _info: {width, height, x, y}, displayName, html, metaData} = node || {};
-      const rowspan = totalHeight - (y + height) + 1;
-      const colspan = width;
-      if (!rowData[y]) rowData[y] = [];
-      const html2 = `<th${rowspan > 1 && ` rowspan="${rowspan}"` || ""}${colspan > 1 && ` colspan="${colspan}"` || ""}>${html || displayName}</th>`;
-      node._info.html = html2;
-      rowData[y][x] = html2;
-      for (const i of i1) {
-        rc2(i);
-      }
-    }
-
-    rc2(charting);
-    console.log(JSON.stringify(charting, null, "  "));
-    console.log(JSON.stringify({totalWidth, totalHeight}, null, "  "));
-
-    const hd2 = rowData.map(x => x.join("")).join("")
-    const hd = items
-        .map(({displayName, html, metaData}: any) => {
-          return `<th>${html || displayName}</th>`
-        })
-        .join("");
-    console.log(JSON.stringify(rowData));
-
-    return `
-<table>
-  <thead>
-     <tr>${hd2}</tr>
-  </thead>
-</table>`
-  }
-
-  psChartingItems() {
-    debugger;
-  }
-
-  psMetaData() {
-    debugger;
-  }
-
-  html() {
-    const l = this
-        .def
-        .map(t => {
-          const {tag, article, panel, charting, form} = t;
-          return this.psPanel(t);
-        });
-    return `<div>${l.join("")}</div>`;
-  }
-}
-
 @Component({
-  components: {Choice, WnFile}
+  components: {Choice, WnFile, GenPrint}
 })
 export default class Print extends Vue {
-  @Prop() private msg!: string;
-
   chartingDef = chartingDef;
-
-  html() {
-    const hd = new HandleDef(chartingDef);
-    return hd.html();
-  }
 }
 
 </script>
@@ -173,7 +54,6 @@ export default class Print extends Vue {
   .el-card {
     margin: 10px;
   }
-
 }
 
 </style>
---
+++ client/src/views/GenPrint.vue
@@ -0,0 +1,125 @@
+<template>
+  <div v-html="html()"/>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+import ElementUI from 'element-ui';
+
+Vue.prototype.$ELEMENT = {size: 'small', zIndex: 3000};
+Vue.use(ElementUI);
+
+@Component({
+  components: {}
+})
+export default class GenPrint extends Vue {
+  @Prop() private def: any;
+
+  psLabel(article: any) {
+    if (!article) return "";
+    const {text, html} = article;
+    if (html) return html;
+  }
+
+  psPanel(t: any) {
+    if (!t) return "";
+
+    const {tag, article, form, charting} = t;
+    if (tag !== "panel") {
+      return `<div>should be panel.</div>`
+    }
+
+    if (article || form || charting)
+      return [
+        this.psLabel(article),
+        this.psForm(form),
+        this.psCharting(charting)
+      ].join("");
+
+    return `<div style="border: 1px solid red">${JSON.stringify(t)}</div>`;
+  }
+
+  psForm(form: any) {
+    if (!form) return "";
+    const {fields = []} = form;
+    return fields
+        .map(({label, text, template}: any) => {
+          return `<div><div>${label}</div><div>${text}</div></div>`
+        })
+        .join("");
+  }
+
+  psCharting(charting: any) {
+    if (!charting) return "";
+    const {items = []} = charting;
+
+    function rc(node: any, pos: any): any {
+      const {items: i1 = []} = node || {};
+      let width = 0, height = 1;
+      const {x = 0, y = 0} = pos;
+      for (const i of i1) {
+        const {width: w2, height: h2} = rc(i, {x: x + width, y: y + 1});
+        width += w2;
+        height = Math.max(height, h2 + 1);
+      }
+      width = Math.max(1, width);
+      node._info = {width, height, x, y};
+      return {width, height, x, y};
+    }
+
+    const {width: totalWidth, height: totalHeight} = rc(charting, {});
+    const rowData: any[] = [];
+
+    function rc2(node: any): any {
+      const {items: i1 = [], _info: {width, height, x, y}, displayName, html, metaData} = node || {};
+      const rowspan = totalHeight - (y + height) + 1;
+      const colspan = width;
+      if (!rowData[y]) rowData[y] = [];
+      const html2 = `<th${rowspan > 1 && ` rowspan="${rowspan}"` || ""}${colspan > 1 && ` colspan="${colspan}"` || ""}>${html || displayName}</th>`;
+      node._info.html = html2;
+      rowData[y][x] = html2;
+      for (const i of i1) {
+        rc2(i);
+      }
+    }
+
+    rc2(charting);
+    console.log(JSON.stringify(charting, null, "  "));
+    console.log(JSON.stringify({totalWidth, totalHeight}, null, "  "));
+
+    const hd2 = rowData
+        .map(x => x.join(""))
+        .map(x => `<tr>${x}</tr>`)
+        .join("")
+    console.log(JSON.stringify(rowData));
+
+    return `
+<table>
+  <thead>
+     <tr>${hd2}</tr>
+  </thead>
+</table>`
+  }
+
+  psChartingItems() {
+    debugger;
+  }
+
+  psMetaData() {
+    debugger;
+  }
+
+  html() {
+    const l = this
+        .def
+        .map((t: any) => {
+          const {tag, article, panel, charting, form} = t;
+          return this.psPanel(t);
+        });
+    return `<div>${l.join("")}</div>`;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
